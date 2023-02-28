bdff2881f2ce869b2e4594f67ea513062013d495
rober <rober@DESKTOP-SOL8K4P>
2023-02-28 17:56:35 +0100

    Metodo2

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -12,6 +12,7 @@ public class AleatorioEmple {
 	}
 
 	public static void leer() throws IOException {
+		//Metodo2
 		File fichero = new File("AleatorioEmple.dat");
 		RandomAccessFile file = new RandomAccessFile(fichero, "r");
 		char cad[] = new char[10], aux;
